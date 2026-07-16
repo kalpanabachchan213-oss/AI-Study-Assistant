5c75f5151cd0fe6f0b46b1ba6d0d4472417a208b
Kalpana yadav <kalpanabachchan213@gmail.com>
2026-07-16 10:47:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-document.querySelector("button").addEventListener("click", function () {
-    alert("Welcome to AI Study Assistant!");
-});
+
+// Welcome message
+window.onload = function(){
+    console.log("AI Study Assistant Website Loaded");
+};
+
+
+// AI Button
+let aiButton = document.querySelector("#assistant button");
+
+if(aiButton){
+    aiButton.onclick = function(){
+        alert("Hello! Ask your AI Study Question.");
+    };
+}
+
+
+// Contact Form
+let form = document.querySelector("form");
+
+if(form){
+    form.onsubmit = function(event){
+        event.preventDefault();
+        alert("Your message has been sent!");
+    };
+}
